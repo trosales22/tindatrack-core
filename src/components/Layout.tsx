7befad067e6142a514c68d2ef1adbe0d7c57b9b8
Tristan Rosales <tristanrosales0@gmail.com>
2025-04-24 22:09:32 +0800

    Add margin bottom in Layout component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -9,7 +9,7 @@ const Layout = ({ children }: LayoutProps) => {
   return (
     <div className="flex flex-col h-screen">
       <Header />
-      <main className="flex-1 overflow-y-auto mt-20 p-4">
+      <main className="flex-1 overflow-y-auto mt-20 mb-15 p-4">
       {children}
       </main>
     </div>
